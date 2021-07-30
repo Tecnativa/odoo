5b5fabd77312df19f3a2972c7298097ea1ba4cf3
Thibault Delavallée <tde@odoo.com>
2021-07-30 09:42:42 +0200

    [IMP] base: avoid double formatting in partner 'email_formatted' field
    PURPOSE
    
    Be defensive when dealing with email fields, notably when having multi-emails
    or email field containing an already-formatted email.
    
    SPECIFICATIONS
    
    Main fix in this commit: fix multiple nested formatting in 'email_formatted'
    computation for <res.partner>. Other use cases are mainly left untouched as
    we let users deal with their input. In summary :
    
      * double format: if email already holds a formatted email like
        'Name' <email@domain.com> we should not use it as it to compute
        email formatted like "Name <'Name' <email@domain.com>>";
      * multi emails: sometimes this field is used to hold several addresses
        like email1@domain.com, email2@domain.com. We currently let this value
        untouched, but remove any formatting from multi emails;
      * invalid email: if something is wrong, better keep it in email_formatted
        than harcoding "False". Indeed this eases management and understanding
        of failures at mail.mail, mail.notification and mailing.trace level;
    
    Task-2612945 (Mail: Defensive email formatting)

--- odoo/tools/mail.py
+++ odoo/tools/mail.py
@@ -526,6 +526,18 @@ def email_normalize(text, force_single=True):
         return False
     return emails[0].lower()
 
+def email_normalize_all(text):
+    """ Tool method allowing to extract email addresses a text input and return
+    normalized version of all found emails. If no email is found, a void list
+    is returned.
+    e.g. if email is 'tony@e.com, "Tony2" <tony2@e.com' returned result is either
+    ['tony@e.com, tony2@e.com']
+    :return list: list of normalized emails found in text
+    """
+    if not text:
+        return []
+    emails = email_split(text)
+    return list(filter(None, [email_normalize(email) for email in emails]))
 
 def email_domain_extract(email):
     """ Extract the company domain to be used by IAP services notably. Domain
