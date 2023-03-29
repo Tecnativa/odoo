29c4dba6657371026668eba96bb82a2c6bdd4477
David <david.vidal@tecnativa.com>
2023-03-29 11:53:24 +0200

    [OU] account: speedup destination_account_id compute

--- addons/account/models/account_payment.py
+++ addons/account/models/account_payment.py
@@ -408,6 +408,22 @@ class AccountPayment(models.Model):
     @api.depends('journal_id', 'partner_id', 'partner_type', 'is_internal_transfer')
     def _compute_destination_account_id(self):
         self.destination_account_id = False
+        receivable_account_dict = {
+            comp:  self.env['account.account'].search([
+                ('company_id', '=', comp.id),
+                ('internal_type', '=', 'receivable'),
+                ('deprecated', '=', False),
+            ], limit=1)
+            for comp in self.company_id
+        }
+        payable_account_dict = {
+            comp:  self.env['account.account'].search([
+                ('company_id', '=', comp.id),
+                ('internal_type', '=', 'payable'),
+                ('deprecated', '=', False),
+            ], limit=1)
+            for comp in self.company_id
+        }
         for pay in self:
             if pay.is_internal_transfer:
                 pay.destination_account_id = pay.journal_id.company_id.transfer_account_id
@@ -416,21 +432,13 @@ class AccountPayment(models.Model):
                 if pay.partner_id:
                     pay.destination_account_id = pay.partner_id.with_company(pay.company_id).property_account_receivable_id
                 else:
-                    pay.destination_account_id = self.env['account.account'].search([
-                        ('company_id', '=', pay.company_id.id),
-                        ('internal_type', '=', 'receivable'),
-                        ('deprecated', '=', False),
-                    ], limit=1)
+                    pay.destination_account_id = receivable_account_dict[pay.company_id]
             elif pay.partner_type == 'supplier':
                 # Send money to pay a bill or receive money to refund it.
                 if pay.partner_id:
                     pay.destination_account_id = pay.partner_id.with_company(pay.company_id).property_account_payable_id
                 else:
-                    pay.destination_account_id = self.env['account.account'].search([
-                        ('company_id', '=', pay.company_id.id),
-                        ('internal_type', '=', 'payable'),
-                        ('deprecated', '=', False),
-                    ], limit=1)
+                    pay.destination_account_id = payable_account_dict[pay.company_id]
 
     @api.depends('partner_bank_id', 'amount', 'ref', 'currency_id', 'journal_id', 'move_id.state',
                  'payment_method_id', 'payment_type')
