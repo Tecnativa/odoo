72f7a93580d35a2129584522a0c533a385108a35
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2025-04-08 08:14:59 +0200

    [FIX] web_editor: prevent background image loss after editing in the website editor
    **Problem**:
    When editing a block with a background image, the image is visible during
    the editing process.
    However, once the user saves the page, the background image is removed,
    even though it was displayed correctly before saving.
    This happens because the editor temporarily uses base64-encoded data for
    the background image and fails to persist the image URL after saving.
    
    **Steps to reproduce**:
    1. Add a block with a background image.
    2. Edit the background image using the Odoo website editor.
    3. While editing, the background is visible and looks correct.
    4. Save the page.
    5. After saving, the background image is lost (set to null).
    
    **Solution**
    The issue occurs because, after editing, the editor does not properly
    handle or persist the final background image URL when saving.
    This fix ensures that background images, regardless of whether they
    are base64 or URLs, are correctly saved and applied.
    The DOM is now updated consistently with the correct final image,
    preventing the loss of the background image after saving.
    
    TT55844

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -893,24 +893,25 @@ const Wysiwyg = Widget.extend({
                     // id parameters.
                     return;
                 }
-                // Frequent media changes or page reloads may trigger a save request  
-                // without removing the `o_modified_image_to_save` class, causing a traceback  
-                // on the next save since the element loses its base64 `src`.  
+                let imageSrc = isBackground ? el.dataset.bgSrc : el.getAttribute('src');
+                const isImageAlreadySaved = !imageSrc || !imageSrc.startsWith("data:");
+                // let newAttachmentSrc = imageSrc;
+                // Frequent media changes or page reloads may trigger a save request
+                // without removing the `o_modified_image_to_save` class, causing a traceback
+                // on the next save since the element loses its base64 `src`.
                 // If the image isn't already saved, a new copy is created.
-                let newAttachmentSrc = el.getAttribute('src');
-                const isImageAlreadySaved = !newAttachmentSrc || !newAttachmentSrc.startsWith("data:");
+                let newAttachmentSrc = imageSrc;
                 if (!isImageAlreadySaved) {
-                    // Modifying an image always creates a copy of the original, even if
-                    // it was modified previously, as the other modified image may be used
-                    // elsewhere if the snippet was duplicated or was saved as a custom one.
                     newAttachmentSrc = await this._rpc({
                         route: `/web_editor/modify_image/${encodeURIComponent(el.dataset.originalId)}`,
                         params: {
                             res_model: resModel,
                             res_id: parseInt(resId),
-                            data: (isBackground ? el.dataset.bgSrc : el.getAttribute('src')).split(',')[1],
-                            mimetype: (isBackground ? el.dataset.mimetype : el.getAttribute('src').split(":")[1].split(";")[0]),
-                            name: (el.dataset.fileName ? el.dataset.fileName : null),
+                            data: imageSrc.split(',')[1],
+                            mimetype: isBackground
+                                ? el.dataset.mimetype
+                                : imageSrc.split(":")[1].split(";")[0],
+                            name: el.dataset.fileName || null,
                         },
                     });
                 }
